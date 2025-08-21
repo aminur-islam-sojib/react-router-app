88b4deeb673e924297ba21f2ed7733026a296efc
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-08-21 17:23:25 +0600

    add footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,61 @@
+import { NavLink } from 'react-router-dom';
+
 const Footer = () => {
   return (
     <>
-      <div>
-        <h1>Hello footer </h1>
+      <div className=" grid grid-cols-4  bg-green-950 text-white p-7 gap-5 ">
+        <div className=" col-span-2">
+          <h1 className=" font-bold text-3xl">ATARAXIS</h1>
+          <p className=" my-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+            mollitia repellat voluptas eius. Modi vel cum architecto mollitia
+            laborum vero?
+          </p>
+          <button className=" font-medium cursor-pointer p-4 outline-white outline-2 hover:bg-white hover:text-green-950 transition-all duration-300 mt-4">
+            BACK TO TOP
+          </button>
+        </div>
+        <div>
+          <h1 className=" font-bold text-2xl">Site Map</h1>
+          <ul className=" flex flex-col gap-2 mt-4  ">
+            <NavLink
+              to="/Home"
+              className={({ isActive }) =>
+                ` ${isActive ? 'text-orange-500' : 'text-amber-50'}`
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/About"
+              className={({ isActive }) =>
+                ` ${isActive ? 'text-orange-500' : 'text-amber-50'}`
+              }
+            >
+              About
+            </NavLink>
+            <NavLink
+              to="/Contact"
+              className={({ isActive }) =>
+                ` ${isActive ? 'text-orange-500' : 'text-amber-50'}`
+              }
+            >
+              Contact
+            </NavLink>
+          </ul>
+        </div>
+        <div>
+          <h1 className=" text-2xl font-bold ">Legal</h1>
+          <ul className=" flex flex-col gap-2 mt-4  ">
+            <NavLink to="/Privacy">Privacy Policy</NavLink>
+            <NavLink to="/Terms">Terms of Service</NavLink>
+            <NavLink to="/Cookies">Cookie Policy</NavLink>
+          </ul>
+        </div>
       </div>
+      <footer className=" bg-orange-950 text-white p-4 text-center">
+        <p className="text-sm">© 2023 Ataraxis. All rights reserved.</p>
+      </footer>
     </>
   );
 };
